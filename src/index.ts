647dd837ad941ce2d14f1b6d7366853df960ee12
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2025-01-16 21:49:42 +0530

    Make Initial Setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
