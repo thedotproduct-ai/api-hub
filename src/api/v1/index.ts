165b57695087adb2332e0a2e5f44967d97cc75fc
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2025-01-19 01:23:12 +0530

    Setup Client Token Generation

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import authRoutes from "../../modules/auth/routes";
 import todoRoutes from "../../modules/todoapp/routes";
 
 const app = express();
 app.use("/todo", todoRoutes);
+app.use("/auth", authRoutes);
 
 export default app;
--- src/common/utils/globals.ts
+++ src/common/utils/globals.ts
@@ -1,3 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
 
-export const prisma = new PrismaClient();
+export const getUserId = (req: Request) => {
+  const cookie = req.cookies;
+  let userId = cookie["userId"];
+  if (!userId || userId === "") {
+    userId = uuidv4();
+  }
+  return userId;
+};
+
+// Set the cookies for 100 years
+export const setCookie = (res: Response, userId: string) => {
+  res.cookie("userId", userId, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+    maxAge: 100 * 365 * 24 * 60 * 60 * 1000,
+  });
+};
